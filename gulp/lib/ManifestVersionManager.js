9e3e15d8574540d20ea2047b3da542349a51d121
eiurur <sigsig_kbeatodenmania@yahoo.co.jp>
2018-05-04 11:29:49 +0900

    [Fix] - npm script of zip does not work properly

--- gulp/lib/ManifestVersionManager.js
+++ gulp/lib/ManifestVersionManager.js
@@ -1,4 +1,4 @@
-module.exports =  class MenifestVersionManager {
+module.exports = class MenifestVersionManager {
   constructor(versionType, version) {
     this.versionType = versionType;
     this.version = version;
@@ -8,22 +8,25 @@ module.exports =  class MenifestVersionManager {
     let v = this.version.split('.');
     switch (this.versionType) {
       case 'major':
-        v[0] = (v[0] - 0) + 1;
+        v[0] = v[0] - 0 + 1;
         v[1] = 0;
         v[2] = 0;
         break;
       case 'minor':
-        v[1] = (v[1] - 0) + 1;
+        v[1] = v[1] - 0 + 1;
         v[2] = 0;
         break;
       case 'patch':
-         v[2] = (v[2] - 0) + 1;
+        v[2] = v[2] - 0 + 1;
         break;
-      default: break;
+      default:
+        break;
     }
     this.version = v.join('.');
     return this;
   }
 
-  getVersion() { return this.version; }
+  getVersion() {
+    return this.version;
+  }
 };
--- gulp/tasks/update_manifest_version.js
+++ gulp/tasks/update_manifest_version.js
@@ -4,10 +4,13 @@ const { argv } = require('yargs');
 
 const ManifestVersionManager = require('../lib/ManifestVersionManager');
 const manifest = require('../../manifest.json');
-const newVersion = new ManifestVersionManager(argv.version, manifest.version).update().getVersion();
+const newVersion = new ManifestVersionManager(argv.versions, manifest.version)
+  .update()
+  .getVersion();
 
 gulp.task('update_manifest_version', function() {
-  return gulp.src('./manifest.json')
-    .pipe($.jsonEditor({version: newVersion}))
+  return gulp
+    .src('./manifest.json')
+    .pipe($.jsonEditor({ version: newVersion }))
     .pipe(gulp.dest('./'));
 });
